b4f090420a2baa922fd83564edfa8c3cc4bac7fe
stacks-of-data <coder64bit@gmail.com>
2024-07-11 21:13:57 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import "./App.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./App.css";
 import SliderComponent from "./SliderComponent";
 
 function App() {
--- src/SliderComponent.tsx
+++ src/SliderComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import Slider from "react-slick";
 
 export default function SliderComponent() {
@@ -10,32 +10,22 @@ export default function SliderComponent() {
     slidesToShow: 1,
     slidesToScroll: 1,
     waitForAnimate: false,
-    autoplay: true,
-    autoplaySpeed: 4000,
     infinite: true,
     arrows: false,
+    appendDots: (dots: ReactNode) => (
+      <div>
+        <ul style={{ margin: "0px" }}> {dots} </ul>
+      </div>
+    ),
   };
   return (
     <div style={{ position: "relative" }}>
       <Slider ref={slider} {...settings}>
         <div>
-          <h3>1</h3>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-		  <img src="example.jpg" width={800} height={600} />
+          <img src="example.jpg" width={"100%"} height={800} />
         </div>
         <div>
-          <h3>2</h3>
-          <img src="example.jpg" width={800} height={600} />
+          <img src="example.jpg" width={"100%"} height={800} />
         </div>
       </Slider>
       <button
